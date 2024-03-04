7f2d7dde78f505cf4eb384f25ac56490b594572c
batuncer <tuncerbaki@gmail.com>
2024-03-04 13:33:11 +0000

    rds terraform is added

---
+++ RDS/terraform/variable.tf
@@ -0,0 +1,21 @@
+
+variable "aws_region" {
+  description = "The AWS region where the RDS instance will be deployed"
+  default     = "us-west-2"
+}
+
+variable "aws_access_key" {
+  description = "AKIAX2XSINLZYFYZ74NH"
+}
+
+variable "aws_secret_key" {
+  description = "TaLkOfBRTtF0gVof907K5qn2Cy/9lN8McEc2b8vy"
+}
+
+variable "db_username" {
+  description = "baki"
+}
+
+variable "db_password" {
+  description = "Tuncer0534."
+}
