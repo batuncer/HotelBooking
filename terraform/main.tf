70e21030d16983a1958000bf5dd74f9f58e265a3
batuncer <tuncerbaki@gmail.com>
2024-03-04 19:07:18 +0000

    remote is added

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,13 +26,14 @@ resource "aws_key_pair" "key_pair" {
 resource "local_file" "private_key" {
   content  = tls_private_key.rsa_4096.private_key_pem
   filename = var.key_name
+  file_permission = "0400" # Dosya iznini 400 olarak ayarla
 }
 
 resource "aws_security_group" "instance_security_group" {
   name        = "instance_security_group"
   description = "Security group for instance"
 
-  ingress {
+   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -44,6 +45,26 @@ resource "aws_security_group" "instance_security_group" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
   }
 
   egress {
@@ -61,6 +82,23 @@ resource "aws_instance" "public_instance" {
   security_groups        = [aws_security_group.instance_security_group.name]
 
   tags = {
-    Name = "cfy hotel"
+    Name = "cfy_hotel_instance"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = local_file.private_key.filename
+      host        = aws_instance.public_instance.public_ip
+    }
+
+    inline = [
+      "sleep 30", # Bekleme süresi ekleyin
+      "sudo yum update -y",
+      "sudo yum install docker -y",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker"
+    ]
   }
 }
