bd479a83d0ba56bb3e6cca9c4ab3cbb16b336af7
Baki Tuncer <98457585+batuncer@users.noreply.github.com>
2024-05-13 15:38:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,10 +34,9 @@ app.get("/", function (req, res, next) {
   res.render("home");
 });
 
-// app.listen(SERVER_PORT, () => {
-//   console.info(`Server started at http://localhost:${SERVER_PORT}`);
-// });
-module.exports = app;
+app.listen(SERVER_PORT, () => {
+  console.info(`Server started at http://localhost:${SERVER_PORT}`);
+});
 
 
 
